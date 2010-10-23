d451d7d95efd6693966a04eaeed0e2ccb40d5651
schamane <schamane@myeburg.net>
2010-10-23 10:30:17 +0200

    remove unneeded require in test file. Tested with current node 0.3.0-pre master

--- test.js
+++ test.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
 
-var sys = require('sys'),
-	Syslog = require('./nodesyslog').Syslog;
-	
+	var Syslog = require('./nodesyslog').Syslog;
+
 	Syslog.init("node-syslog-test", Syslog.LOG_PID | Syslog.LOG_ODELAY, Syslog.LOG_NEWS);
 	Syslog.log(Syslog.LOG_INFO,"news info log test");
 	Syslog.log(Syslog.LOG_ERR,"news log error test");
 	Syslog.log(Syslog.LOG_DEBUG,"Last log message as debug:"+new Date());
-	
+
 
