e79d4004a22eb330c0bcfdb60150513a4f993f6f
schamane <schamane@myeburg.net>
2012-06-02 14:14:57 +0200

    fix issue #16

--- test.js
+++ test.js
@@ -1,10 +1,8 @@
-#!/bin/env node
+var Syslog = require('./node-syslog');
 
-	var Syslog = require('./node-syslog');
-
-	Syslog.init("node-syslog-test", Syslog.LOG_PID | Syslog.LOG_ODELAY, Syslog.LOG_LOCAL0);
-	Syslog.log(Syslog.LOG_INFO, "news info log test");
-	Syslog.log(Syslog.LOG_ERR, "news log error test");
-	Syslog.log(Syslog.LOG_DEBUG, "Last log message as debug: " + new Date());
+Syslog.init("node-syslog-test", Syslog.LOG_PID | Syslog.LOG_ODELAY, Syslog.LOG_LOCAL0);
+Syslog.log(Syslog.LOG_INFO, "news info log test");
+Syslog.log(Syslog.LOG_ERR, "news log error test");
+Syslog.log(Syslog.LOG_DEBUG, "Last log message as debug: " + new Date());
 
 
